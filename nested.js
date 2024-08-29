c763da449ee5d8ba89c417210fabc5b5704aca98
capnemo20 <z.sujon20@outlook.com>
2024-08-29 23:51:38 +0100

    nested if else

---
+++ nested.js
@@ -0,0 +1,27 @@
+const money = 30;
+if(money>300){
+    console.log("bro you are rich")
+}
+else{
+    if(money>100){
+        console.log("tui gorib o na borolok o na");
+    }
+    else{
+        if(money>50){
+            console.log("tui to foinni");
+        }
+        else{
+            if(money>20){
+                console.log("tui to beda rich")
+            }
+            else{
+                if(money>5){
+                    console.log("tor kopal bhalo");
+                }
+                else{
+                    console.log("tui mara kha");
+                }
+            }
+        }
+    }
+}
