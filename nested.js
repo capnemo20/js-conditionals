e0e765b472757cd1c49623d4816ba80870b8e335
capnemo20 <z.sujon20@outlook.com>
2024-08-30 00:42:06 +0100

    nested if else 2

--- nested.js
+++ nested.js
@@ -1,27 +1,47 @@
 const money = 30;
 if(money>300){
-    console.log("bro you are rich")
+    // console.log("bro you are rich")
 }
 else{
     if(money>100){
-        console.log("tui gorib o na borolok o na");
+        // console.log("tui gorib o na borolok o na");
     }
     else{
         if(money>50){
-            console.log("tui to foinni");
+            // console.log("tui to foinni");
         }
         else{
             if(money>20){
-                console.log("tui to beda rich")
+                // console.log("tui to beda rich")
             }
             else{
                 if(money>5){
-                    console.log("tor kopal bhalo");
+                    // console.log("tor kopal bhalo");
                 }
                 else{
-                    console.log("tui mara kha");
+                    // console.log("tui mara kha");
                 }
             }
         }
     }
 }
+
+let price = 500;
+const isLeader = true;
+if(isLeader===true){
+    // console.log(0);
+}
+else{
+    price+=100;
+}
+// console.log(price);
+
+price = isLeader===true?0:price=price+100;
+
+// console.log(price= isLeader===true?0:price=price+100);
+
+price = isLeader === true? 
+    price>1000?
+        1000/2:0:
+            price+1000;
+
